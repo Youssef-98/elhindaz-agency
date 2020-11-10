6e57fc2df1bad696c179cd534bbba78f537c2a21
Youssef-EH <68298847+Youssef-EH@users.noreply.github.com>
2020-11-10 17:34:06 +0100

    add about page

---
+++ src/pages/about-us.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { useStaticQuery, graphql } from "gatsby"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+import { Wrapper, Image, BottomEdgeDown, BottomEdgeUp } from "./pageStyles/pageStyles"
+import { COLORS } from "../constants"
+
+const AboutUs = (props) => {
+    const {wpcontent: {
+      page: {
+        aboutUsMeta: {
+          aboutUsDescription,
+          aboutUsPageHeaderPicture
+        }
+      }
+    }} = useStaticQuery(graphql`
+    query {
+        wpcontent{
+          page(id: "about-us", idType: URI) {
+            aboutUsMeta {
+              aboutUsDescription
+              aboutUsPageHeaderPicture {
+                  sourceUrl
+                  imageFile {
+                    childImageSharp {
+                      fluid(quality: 100) {
+                        ...GatsbyImageSharpFluid_withWebp
+                      }
+                    }
+                  }
+                altText
+              }
+            }
+          }
+        }
+      }
+    `)
+
+    return (
+        <Layout>
+          <Wrapper descriptionColor={COLORS.PRIMARY}>
+            <div className="banner">
+              <Image fluid={aboutUsPageHeaderPicture.imageFile.childImageSharp.fluid} alt={aboutUsPageHeaderPicture.altText} />
+              <BottomEdgeDown color={COLORS.PRIMARY} /> 
+            </div>
+            <div className="description"> 
+              <h2>About Us</h2>
+              <p>{aboutUsDescription}</p>
+              <BottomEdgeUp color={COLORS.BLACK} />
+            </div>
+          </Wrapper>
+        </Layout>
+    );
+}
+
+export default AboutUs;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,9 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
-import {
-  Wrapper,
-  Image,
-  Artist,
-  BottomEdgeDown,
-  BottomEdgeUp,
-} from "./pageStyles/pageStyles"
+import { Wrapper, Image, Artist, BottomEdgeDown, BottomEdgeUp } from "./pageStyles/pageStyles"
 import { COLORS } from "../constants"
 
 const IndexPage = () => {
